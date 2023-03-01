6a2ed194a1d085f433b04da44c12dbd0d04d1c2a
ub <s4119@noreply.codeberg.org>
2023-03-01 22:22:38 +0000

    Update 'v1/parseJSON.php'

--- v1/parseJSON.php
+++ v1/parseJSON.php
@@ -1,5 +1,5 @@
 <?php
-$people_json = file_get_contents('http://jwist.125mb.com/api/v1/reverseString.php?content=tacocat');
+$people_json = file_get_contents('http://jwist.125mb.com/api/v1/reverseString.php?content=this is reversed');
 $decoded_json = json_decode($people_json, false);
 echo $decoded_json->reversed;
 // Monty 
