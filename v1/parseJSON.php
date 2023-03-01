3d5e14b4778a7f3d357d3e6e9aa7b73ef540758a
ub <s4119@noreply.codeberg.org>
2023-03-01 22:27:56 +0000

    Update 'v1/parseJSON.php'

--- v1/parseJSON.php
+++ v1/parseJSON.php
@@ -1,7 +1,11 @@
 <?php
-$people_json = file_get_contents('http://jwist.125mb.com/api/v1/reverseString?content=%3E?%20;2ravym$%20ohce%20;%27olleh%27%20=%202ravym$%20php?%3C');
+$people_json = file_get_contents('http://jwist.125mb.com/api/v1/reverseString?content=reversed-desrever');
+$people_json2 = file_get_contents('http://jwist.125mb.com/api/v1/getVersion');
 $decoded_json = json_decode($people_json, false);
+$decoded_json2 = json_decode($people_json2, false);
 echo $decoded_json->reversed;
+echo $decoded_json2->phpversion;
+
 // Monty 
 // echo $decoded_json->email;
 // monty@something.com 
