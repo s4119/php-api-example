1b8a31094f036b50f008593dcd39e02d81e27e7a
ub <s4119@noreply.codeberg.org>
2023-03-02 23:37:52 +0000

    Update 'v1/textImage.php'

--- v1/textImage.php
+++ v1/textImage.php
@@ -1,23 +1,4 @@
 <?php
-/*
-// Create a 100*30 image
-$im = imagecreate(100, 30);
-
-// White background and blue text
-$bg = imagecolorallocate($im, 255, 255, 255);
-$textcolor = imagecolorallocate($im, 0, 0, 0);
-
-// Write the string at the top left
-imagestring($im, 5, 0, 0, $_GET["text"], $textcolor);
-
-// Output the image
-header('Content-type: image/png');
-
-imagepng($im);
-imagedestroy($im);
-*/
-?>
-<?php
 // Set the content-type
 header('Content-Type: image/png');
 
